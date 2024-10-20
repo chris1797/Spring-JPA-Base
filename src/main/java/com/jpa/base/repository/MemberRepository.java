ca22f9c0b7c65c6483dde4137311832cc314dfa1
chris1797 <jaehun6004@gmail.com>
2024-10-20 23:56:03 +0900

    Member setup

--- src/main/java/com/jpa/base/repository/MemberRepository.java
+++ src/main/java/com/jpa/base/repository/MemberRepository.java
@@ -1,7 +1,19 @@
 package com.jpa.base.repository;
 
+import com.jpa.base.entity.Member;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
 }
