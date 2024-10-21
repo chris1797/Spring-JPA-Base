b3a49c6510140011b37e5fdf76e1b454b3586c49
chris1797 <jaehun6004@gmail.com>
2024-10-21 23:07:00 +0900

    Member test code setup

--- src/test/java/com/jpa/base/repository/MemberRepositoryTest.java
+++ src/test/java/com/jpa/base/repository/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,7 +32,11 @@ class MemberRepositoryTest {
         Member findMember = memberRepository.find(savedId);
         assertEquals(findMember.getId(), member.getId());
         assertEquals(findMember.getUsername(), member.getUsername());
+
+        // 같은 영속성 컨텍스트에서는 id 값이 같으면 같은 인스턴스로 관리되기 때문에 같은 인스턴스로 관리된다.
         assertEquals(findMember, member);
+
+
     }
 
 }
