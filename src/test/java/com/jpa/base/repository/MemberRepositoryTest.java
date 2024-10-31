00d77e33b51a68c8a0d3d82951f6192ceb46abfe
chris1797 <jaehun6004@gmail.com>
2024-11-01 00:53:42 +0900

    feat: Member update test

--- src/test/java/com/jpa/base/repository/MemberRepositoryTest.java
+++ src/test/java/com/jpa/base/repository/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.jpa.base.repository;
 
 import com.jpa.base.domain.Member;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Transactional
 @SpringBootTest
 class MemberRepositoryTest {
 
@@ -16,8 +18,8 @@ class MemberRepositoryTest {
 
 //    @Rollback(false)
     @Test
-    @Transactional
-    void save() {
+    @DisplayName("Member 저장 테스트")
+    void saveTest() {
         // given
         Member member = new Member();
         member.setName("memberA");
@@ -33,6 +35,47 @@ class MemberRepositoryTest {
 
         // 같은 영속성 컨텍스트에서는 id 값이 같으면 같은 인스턴스로 관리되기 때문에 같은 인스턴스로 관리된다.
         assertEquals(findMember, member);
+    }
+
+    @Test
+    @DisplayName("Member 수정 테스트")
+    void updateTest() {
+        // given
+        Member member = new Member();
+        member.setName("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.find(savedId);
+        findMember.setName("memberB");
+
+        // 변경 감지(Dirty Checking)으로 인해 update 쿼리가 자동으로 날아간다.
+        assertEquals(findMember.getName(), "memberB");
+    }
+
+    @Test
+    @DisplayName("Member 수정 실패 테스트")
+    void updateFailTest() {
+        // given
+        Member member = new Member();
+        member.setName("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+
+        // then
+        Member findMember = memberRepository.find(savedId);
+        System.out.println("Before findMember = " + findMember.getName());
+        // 변경 감지(Dirty Checking)으로 인해 update 쿼리가 자동으로 날아간다.
+        findMember.setName("memberB");
+
+        Member findMember2 = memberRepository.find(savedId);
+        System.out.println("After findMember = " + findMember2.getName());
+
+        // 변경 감지(Dirty Checking)으로 인해 update 쿼리가 자동으로 날아간다.
+        assertNotEquals(findMember.getName(), "memberA");
     }
 
 }
