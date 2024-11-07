ac904a4e03ebfdbabdddfa2e6ae7ebb74b9d92af
chris1797 <jaehun6004@gmail.com>
2024-11-08 00:31:12 +0900

    feat: test member join()

--- src/test/java/com/jpa/base/service/MemberServiceTest.java
+++ src/test/java/com/jpa/base/service/MemberServiceTest.java
@@ -1,6 +1,5 @@
 package com.jpa.base.service;
 
-import com.fasterxml.jackson.databind.deser.std.StdKeyDeserializer;
 import com.jpa.base.domain.Member;
 import com.jpa.base.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -34,6 +32,11 @@ class MemberServiceTest {
         Member member = new Member();
         member.setName("chris");
 
+        /*
+         * 실제로는 insert 쿼리가 실행되지 않는다. @Transactional 어노테이션이 붙어있기 때문에
+         * 트랜잭션을 커밋하지 않고 롤백하기 때문이다.
+         * 따라서, 실제로 insert 쿼리가 실행되는지 확인하려면 @Rollback(false)를 붙이거나 em.flush()를 호출해야 한다.
+         */
         // when
         Long saveId = memberService.join(member);
 
