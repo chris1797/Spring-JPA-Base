3afa3ecec0942f4dfa7f3df6d4af2c1979bf8538
chris1797 <jaehun6004@gmail.com>
2024-11-13 00:27:55 +0900

    feat: order()

--- src/main/java/com/jpa/base/service/OrderService.java
+++ src/main/java/com/jpa/base/service/OrderService.java
@@ -1,6 +1,12 @@
 package com.jpa.base.service;
 
+import com.jpa.base.domain.Delivery;
+import com.jpa.base.domain.Member;
 import com.jpa.base.domain.Order;
+import com.jpa.base.domain.OrderItem;
+import com.jpa.base.domain.item.Item;
+import com.jpa.base.repository.ItemRepository;
+import com.jpa.base.repository.MemberRepository;
 import com.jpa.base.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,11 +18,26 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
 
 
     @Transactional
-    public Long save(Order order) {
-        orderRepository.save(order);
-        return order.getId();
+    public Long order(Long memberId, Long itemId, int count) {
+
+        Member member = memberRepository.findById(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문 상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        return orderRepository.save(order);
     }
 }
