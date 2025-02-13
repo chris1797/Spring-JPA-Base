0efabcb26c1105e6fada280636ad9ddbb10af799
chris1797 <jaehun6004@gmail.com>
2025-02-13 23:58:20 +0900

    feat: ManyToMany

--- src/main/java/com/jpa/base/manyToMany/Member.java
+++ src/main/java/com/jpa/base/manyToMany/Member.java
@@ -3,6 +3,10 @@ package com.jpa.base.manyToMany;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
@@ -12,4 +16,8 @@ public class Member {
     public Long id;
 
     public String name;
+
+    // - @OneToMany(mappedBy = "member") : Order 엔티티의 member 필드에 의해 매핑된 것을 의미.
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 }
